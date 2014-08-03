be224ce5a38091166279f1c6b246235146adfe5a
Jackie Lee <jackylee0424@gmail.com>
2014-08-03 07:19:23 -0700

    bug fix

--- software/parsedht.py
+++ software/parsedht.py
@@ -5,6 +5,8 @@ import os
 import requests
 import json
 import pickle
+import thread
+
 
 block_list = []
 machine_name = "old_raspi"
@@ -25,33 +27,38 @@ else:
     with open(os.path.join('data', 'block.blk'), 'wb') as f:
         pickle.dump(block_list, f)
 
-url = "https://timeseriesvisual.firebaseio.com/raw.json"
-device = "/dev/tty.usbmodemfa131"
-ser = serial.Serial(device, 9600, timeout=5, parity="N", bytesize=8)
-
-while True:
-    line = ser.readline()
-    if len(line) == 25:
-		dht22 = map(lambda x:float(x), line.strip().split(' '))
-		output_dhc22 = dict()
-		output_dhc22["timestamp"] = time.time()
-		output_dhc22["data"] = dict()
-		output_dhc22["data"]["%s_humidity"%machine_name] = dht22[0]
-		output_dhc22["data"]["%s_temperaturec"%machine_name] = dht22[1]
-		output_dhc22["data"]["%s_temperaturef"%machine_name] = dht22[2]
-		output_dhc22["data"]["%s_heatindexf"%machine_name] = dht22[3]
-
-		block_list.append(output_dhc22)
-		# create a blank block
-		with open(os.path.join('data', 'block.blk'), 'wb') as output:
-		    pickle.dump(block_list, output, pickle.HIGHEST_PROTOCOL)
-		    print "save it to file"
-
-		print len(block_list)
+def posttofirebase():
+	with open(os.path.join('data', 'block.blk'), 'wb') as output:
+		pickle.dump(block_list, output, pickle.HIGHEST_PROTOCOL)
+		print "save it to file"
 
 		try:
 			# post to firebase
-			response = requests.post(url, data=json.dumps(output_dhc22))
+			response = requests.patch(url, data=json.dumps(dict(raw=block_list)))
 			print response
 		except:
 			print "posting error"
+
+
+if __name__ == "__main__":
+
+	url = "https://timeseriesvisual.firebaseio.com/.json"
+	device = "/dev/tty.usbmodemfd111"
+	ser = serial.Serial(device, 9600, timeout=5, parity="N", bytesize=8)
+
+	while True:
+	    line = ser.readline()
+	    if len(line) == 25:
+			dht22 = map(lambda x:float(x), line.strip().split(' '))
+			#print dht22
+			output_dhc22 = dict()
+			output_dhc22["timestamp"] = time.time()
+			output_dhc22["data"] = dict()
+			output_dhc22["data"]["%s-humidity"%machine_name] = dht22[0]
+			output_dhc22["data"]["%s-temperaturec"%machine_name] = dht22[1]
+			output_dhc22["data"]["%s-temperaturef"%machine_name] = dht22[2]
+			output_dhc22["data"]["%s-heatindexf"%machine_name] = dht22[3]
+
+			block_list.append(output_dhc22)
+			print len(block_list)
+			thread.start_new_thread(posttofirebase,())
